0b60b1e3e8db4a9015c138a605a432d5c2ec4cae
pekpuglia <pepuglia@gmail.com>
2022-12-16 13:42:19 -0300

    kinda works

--- src/base_fsm.rs
+++ src/base_fsm.rs
@@ -5,28 +5,28 @@ use std::collections::HashMap;
 pub trait State {
     fn act(&mut self);
     fn transition_conditions(&self) -> Vec<TransitionOptions>;
+    fn set_next(&mut self, transition_index: usize, next: ValidKey);
 }
 
-pub trait StateWithTransitions: State + Clone {
-    fn add_transitions(&mut self, condition: fn(&Self) -> TransitionOptions);
-}
+//remover end_execution
+#[derive(Debug)]
 pub enum TransitionOptions {
     Stay,
-    Change(usize),
-    EndExecution
+    Change(Option<ValidKey>)
 }
-
 
 pub struct FSM {
     states: HashMap<usize, Box<dyn State>>,
     current: usize
 }
 
+#[derive(Clone, Copy, Debug)]
 pub struct ValidKey {
     k: usize
 }
 
 impl FSM {
+
     fn act(&mut self) {
         self.states.entry(self.current).and_modify(|state| state.act());
     }
@@ -36,17 +36,21 @@ impl FSM {
             .unwrap()
             .transition_conditions()
             .iter()
-            .filter_map(|opt| match opt {
-                TransitionOptions::Stay => None,
-                TransitionOptions::Change(next) => {
-                    self.current = *next;
-                    Some(true)},
-                TransitionOptions::EndExecution => Some(false),
-            })
-            .nth(1)
+            .filter_map(|opt|
+                {
+                    match opt {
+                        TransitionOptions::Stay => None,
+                        TransitionOptions::Change(next) => {
+                            match next {
+                                Some(v) => {self.current = v.k; Some(true)},
+                                None => Some(false)
+                            }
+                        },
+                    }
+                }
+            )
+            .nth(0)
             .unwrap_or(true)
-            ;
-        false
     }
 
     pub fn execute(mut self) {
@@ -55,7 +59,7 @@ impl FSM {
         }
     }
 
-    pub fn new(initial_state: impl StateWithTransitions + 'static) -> (FSM, ValidKey) {
+    pub fn new(initial_state: impl State + 'static) -> (FSM, ValidKey) {
         let map = {
             let mut temp = HashMap::new();
             temp.insert(1 as usize, Box::new(initial_state) as Box<dyn State>);
@@ -64,9 +68,16 @@ impl FSM {
         (FSM { states: map, current: 1 }, ValidKey{ k: 1 })
     }
 
-    pub fn insert_state(&mut self, state: impl StateWithTransitions + 'static) -> ValidKey {
+    pub fn add_transition(&mut self, from: ValidKey, transition_index: usize, to: impl State + 'static) -> ValidKey {
         let next_key = self.states.len() + 1;
-        self.states.insert(next_key, Box::new(state) as Box <dyn State>);
+        self.states.insert(next_key, Box::new(to) as Box <dyn State>);
+
+        self.states
+        .entry(from.k)
+            .and_modify(|state| 
+                state.set_next(transition_index, ValidKey { k: next_key })
+            );
+
         ValidKey { k: next_key }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,39 +4,42 @@ use base_fsm::*;
 #[derive(Clone)]
 struct Counter {
     current: usize,
-    exit_condition: fn(&Counter) -> TransitionOptions
+    next: Option<ValidKey>
 }
 
 impl Counter {
     fn new(max: usize) -> Counter {
-        Counter { current: max, exit_condition: (|c:&Counter| TransitionOptions::Stay)}
+        Counter { 
+            current: max, 
+            next: None 
+        }
     }
 }
 
 impl State for Counter {
     fn act(&mut self) {
         self.current -= 1;
+        println!("{}", self.current);
     }
 
     fn transition_conditions(&self) -> Vec<TransitionOptions> {
-        vec![(self.exit_condition)(self)]
+        vec![
+            match self.current {
+                0 => TransitionOptions::Change(self.next),
+                _ => TransitionOptions::Stay
+            }
+        ]
+    }
+
+    fn set_next(&mut self, transition_index: usize, next: ValidKey) {
+        self.next = Some(next)
     }
 }
-
-impl StateWithTransitions for Counter {
-    fn add_transitions(&mut self, condition: fn(&Self) -> TransitionOptions) {
-        self.exit_condition = condition;
-    }
-}
-
 
 fn main() {
-    let mut c1 = Counter::new(10);
-    c1.add_transitions(|c| match c.current == 0 {
-        true => TransitionOptions::Change(2),
-        false => TransitionOptions::Stay,
-    });
-    //retornar ref mutavel p estado adicionado (p ser possível adicionar transições)
-    let (mut fsm, v1) = base_fsm::FSM::new(c1);
-    fsm.insert_state(Counter::new(20));
+    let (mut fsm, v1) = base_fsm::FSM::new(Counter::new(10));
+
+    
+    
+    fsm.execute();
 }
