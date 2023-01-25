c3b91966d2f64d3b51172b38cb89ce68891831a4
pekpuglia <pepuglia@gmail.com>
2023-01-25 17:59:37 -0300

    set_next macro

--- examples/count_input_fsm/count_input_fsm.rs
+++ examples/count_input_fsm/count_input_fsm.rs
@@ -23,20 +23,32 @@ pub struct CountAndInputFSM {
     current: CountAndInputFSMStateTypes
 }
 
-//automatizar set_next
 //automatizar new, trait FSM
 //automatizar declaração da struct
 //fundir com o fsm_enums!
 
+macro_rules! configure_transitions {
+    ($state:expr, $($transition:expr => $next:expr),+) => {
+        $state
+        $(
+            .set_next($transition, $next)
+        )+
+    };
+}
+
+
 impl CountAndInputFSM {
     pub fn new(starting_number: usize) -> CountAndInputFSM {
-        let start_counter = Counter::new(starting_number)
-            .set_next(CounterTransitions::Zero, CountAndInputFSMStates::Inputter);
+        let start_counter = configure_transitions!(
+            Counter::new(starting_number), CounterTransitions::Zero => CountAndInputFSMStates::Inputter
+        );
+        
 
-        let inp = Inputter::new("selecione o próximo estado", "contador 10", "contador 20")
-            .set_next(InputterTransitions::Transition1, CountAndInputFSMStates::Counter10)
-            .set_next(InputterTransitions::Transition2, CountAndInputFSMStates::Counter20);
-
+        let inp = configure_transitions!(
+            Inputter::new("selecione o próximo estado", "contador 10", "contador 20"),
+                InputterTransitions::Transition1 => CountAndInputFSMStates::Counter10,
+                InputterTransitions::Transition2 => CountAndInputFSMStates::Counter20
+        );
         let counter10 = Counter::new(10);
 
         let counter20 = Counter::new(20);
