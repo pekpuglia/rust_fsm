51ab6b83312a468fddc68894ed5e1f64b56c11b5
pekpuglia <pepuglia@gmail.com>
2023-03-07 22:19:21 -0300

    fix dumb warning

--- examples/no_clone_obj_safe_count_and_input_fsm/main.rs
+++ examples/no_clone_obj_safe_count_and_input_fsm/main.rs
@@ -8,7 +8,7 @@ use count_input_fsm::CountAndInputFSM;
 
 //fazer trait state único
 fn main() {
-    let mut fsm = CountAndInputFSM::new(5);
+    let fsm = CountAndInputFSM::new(5);
     
     fsm.execute();
 }
